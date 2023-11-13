a50e1aa983a4b6c7ae44ddd27ea310cedc58d67b
ketsar28 <muhammadketsar1@gmail.com>
2023-11-13 09:58:33 +0700

    [EIGHTH] starter api gateaway

---
+++ src/main/java/com/express/apigateaway/ApiGateawayApplication.java
@@ -0,0 +1,13 @@
+package com.express.apigateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateawayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/express/apigateaway/ApiGateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.express.apigateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
