0567c0159dc4eb9b1d9508fd1b7a5bf7f374b124
ketsar28 <muhammadketsar1@gmail.com>
2023-11-14 20:29:37 +0700

    [NINTH] add come codes

--- src/main/java/com/express/apigateaway/ApiGateawayApplication.java
+++ src/main/java/com/express/apigateaway/ApiGateawayApplication.java
@@ -2,10 +2,12 @@ package com.express.apigateaway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ApiGateawayApplication {
 
 	public static void main(String[] args) {
